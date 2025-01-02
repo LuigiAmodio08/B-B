80ebc2111979e2c86be93f7a96ed8e3a9792b8b9
LuigiAmodio08 <luigiamodio23@gmail.com>
2025-01-02 01:36:24 +0100

    Add files via upload

---
+++ Gestione BnB/main.py
@@ -0,0 +1,11 @@
+# main.py
+
+from app import BnBApp
+from bnb import BnB
+import tkinter as tk
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    bnb = BnB()
+    app = BnBApp(root, bnb)
+    root.mainloop()
